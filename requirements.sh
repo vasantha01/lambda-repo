7b984e0e6faf4ed22adcedddcbf249fa39c06850
vasantha01 <67585672+vasantha01@users.noreply.github.com>
2021-07-17 21:03:33 +0530

    Delete requirements.sh

--- requirements.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo yum -y update          # Install the latest system updates.
-sudo yum -y install aws-cli # Install the AWS CLI.
-aws --version               # Confirm the AWS CLI was installed.
