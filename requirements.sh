88ff734dc1068cf504ef37392efbb92704103ab6
vasantha01 <67585672+vasantha01@users.noreply.github.com>
2021-07-17 17:06:07 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
-
-unzip awscli-bundle.zip
-
-sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
-
-./awscli-bundle/install -b ~/bin/aws
-
-./awscli-bundle/install -h
-
-aws configure
+sudo yum -y update          # Install the latest system updates.
+sudo yum -y install aws-cli # Install the AWS CLI.
+aws --version               # Confirm the AWS CLI was installed.
