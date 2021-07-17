2de3921a30e9750089e87553190a016891c7d534
vasantha01 <67585672+vasantha01@users.noreply.github.com>
2021-07-17 20:55:56 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 sudo yum -y update          # Install the latest system updates.
 sudo yum -y install aws-cli # Install the AWS CLI.
 aws --version               # Confirm the AWS CLI was installed.
