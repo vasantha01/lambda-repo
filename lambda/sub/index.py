3866d7a6db1f395f0ec2780b8adadb0d10bfb73f
vasantha01 <67585672+vasantha01@users.noreply.github.com>
2022-04-18 14:53:05 +0530

    Update index.py

--- lambda/sub/index.py
+++ lambda/sub/index.py
@@ -10,12 +10,13 @@ def lambda_handler(event, context):
     print('Connecting to the Greenplum Database...')
     conn = psycopg2.connect(**rsparams)
     print(conn)
-    # conn = psycopg2.connect(dbname="aims_bi_dev", user="aimsbi-dev", password="Aimsbi#123", port=5439,host="aims-bi-redshiftcluster-eu-central-1-195644677231.cqxzffz280ih.eu-central-1.redshift.amazonaws.com")
+    conn = psycopg2.connect(dbname="aims_bi_dev", user="aimsbi-dev", password="Aimsbi0123", port=5439,host="aims-bi-redshiftcluster-eu-central-1-195644677231.cqxzffz280ih.eu-central-1.redshift.amazonaws.com")
     print(conn)
     cur = conn.cursor()
     print("redshift connected successfully")
-    bucket_name = "lambda-poc-test1"
-    folder_name = "lambda-poc-test1/aims-bi-app/sql/wipo_emr_ddl_scripts.sql"
+    bucket_name = "ranibucket2"
+    #folder_name = "lambda-poc-test1/aims-bi-app/sql/wipo_emr_ddl_scripts.sql"
+    folder_name = "wipo_emr_ddl_scripts.sql
     data = s3.get_object(Bucket=bucket_name, Key=(folder_name))
     #data = s3.get_object(Bucket='analytics-v1-application-eu-central-1-195644677231/aims-bi-app/sql/', Key='wipo_emr_ddl_scripts.sql')
     print(type(data))
